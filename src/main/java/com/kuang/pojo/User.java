c991eb172a7d2f77d1647fc6a98fccef835137d3
laowang988 <71987215@qq.com>
2021-02-24 17:48:06 +0800

    first commit

---
+++ src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,121 @@
+package com.kuang.pojo;
+import java.util.*;
+public class User {
+    private int id;
+    private String userCode;
+    private String UserName;
+    private String userPassword;
+    private int gender;
+    private  Date birthday;
+    private String phone;
+    private String address;
+    private long userRole;
+    private long createBy;
+    private Date creationDate;
+    private long modifyBy;
+    private Date modifyDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(long userRole) {
+        this.userRole = userRole;
+    }
+
+    public long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public long getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(long modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
